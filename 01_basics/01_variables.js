9b04cd6a5fc66c41c5d8924868db3e1dca53909a
Manas Gupta <73732686+guptamanas@users.noreply.github.com>
2023-11-10 09:12:35 +0000

    variables in js basics

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -11,5 +11,6 @@ home="234";
 home="455";
 console.log(home);
 let a;
+let b;
 console.log(a);
-console.table([x,y,z,a,home]);  //ek sath 1 se jyada variables print krne ke liye in tabular form
+console.table([x,y,z,a,home,b]);  //ek sath 1 se jyada variables print krne ke liye in tabular form
