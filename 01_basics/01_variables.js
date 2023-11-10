00d461c5731387e1cf15952e1ae8a5280d0aa003
Manas Gupta <73732686+guptamanas@users.noreply.github.com>
2023-11-10 09:10:30 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const x="hello";
+//x="manas";  cannot chnage const
+console.log(x);
+let y="1234";
+y=0974
+console.log(y);
+var z="jai"  //never use var because it does not follow block or scope
+z="hari"
+console.log(z);
+home="234";
+home="455";
+console.log(home);
+let a;
+console.log(a);
+console.table([x,y,z,a,home]);  //ek sath 1 se jyada variables print krne ke liye in tabular form
