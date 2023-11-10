00f4a25ec9158a5101a95fa0ffbe6a0a869184bd
Manas Gupta <73732686+guptamanas@users.noreply.github.com>
2023-11-10 13:30:44 +0000

    operaions

---
+++ 01_basics/operations_js.js
@@ -0,0 +1,25 @@
+let x=4;
+let negval= -x;
+/*
+console.log(negval);
+console.log(2+1);
+console.log(2*1);
+console.log(4/2);
+console.log(2**2);
+console.log(8%2);
+*/
+
+let str1="hello";
+let str2="world";
+console.log(str1 + str2);  //it combines both strings
+
+console.log("1"+2);  //12
+console.log("2"+1);  //21
+console.log("2"+3+3+5);  //2335
+console.log(2+3+"3");  //53
+console.log(2+"3"+3);  //233
+console.log((3+4)*5%5);  0
+
+console.log(+true);  //1
+console.log(+"");  //0
+
